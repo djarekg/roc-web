da4e281e30685c7ad8a35697f8be464628099d05
Dustin Griffith <dustingriffith@outlook.com>
2022-12-17 19:29:24 -0500

    change

--- apps/callcenter/src/app/stakeholder/prescriber/store/selectors/prescribers.selectors.ts
+++ apps/callcenter/src/app/stakeholder/prescriber/store/selectors/prescribers.selectors.ts
@@ -74,7 +74,7 @@ export const selectViewModel = createSelector(
   selectPrescribersFlatten,
   selectPagination,
   (loading, prescribers, pagination): ViewModel<PrescriberList> => ({
-    loading: true,
+    loading,
     entities: prescribers,
     ...pagination,
   }),
